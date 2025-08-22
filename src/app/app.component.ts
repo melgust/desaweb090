80460d3e0436b99eb1bdd06d4ded147f6cebfa21
esianc <ericksianc@gmail.com>
2025-08-21 21:04:23 -0600

    carga con la tabla editada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,11 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {provideNativeDateAdapter} from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
 import swal from 'sweetalert';
+import { Item } from './models/item';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +21,13 @@ import swal from 'sweetalert';
   providers: [provideNativeDateAdapter()],
   imports: [CommonModule, ReactiveFormsModule, HttpClientModule, MatCardModule, 
     MatSlideToggleModule,
-    MatFormFieldModule, MatInputModule, MatDatepickerModule
+    MatFormFieldModule, MatInputModule, MatDatepickerModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatTableModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
@@ -28,10 +38,21 @@ export class AppComponent {
     message: ['', [Validators.required, Validators.minLength(5)]],
   });
 
+  items: Item[] = [];
+
   constructor(
     private fb: FormBuilder,
     private messageService: MessageService) {
-      
+      let item = new Item();
+      item.id = 1;
+      item.description = 'Curso para aprender a analizar los requerimientos';
+      item.name = 'Analisis de Sistemas II';
+      this.items.push(item);
+      item = new Item();
+      item.id = 1;
+      item.description = 'Curso para aprender sobre micro controladores';
+      item.name = 'Arquitectura de Computadores';
+      this.items.push(item);
     }
 
   onSubmit() {
@@ -45,11 +66,26 @@ export class AppComponent {
         },
         error: (err) => console.error('❌ Error sending message:', err),
       });
+      
     }
   }
 
   showMessage() {
     swal("Good job!", "You clicked the button!", "success");
+  }
+
+  displayedColumns = ['id', 'name', 'description', 'actions'];
+
+  openDialog(item?: Item) {
+    if (item) {
+      swal("Good job!", "You clicked the edit button!", "information");
+    } else {
+      swal("Good job!", "You clicked the add button!", "success");
+    }
+  }
+
+  delete(item: Item) {
+    swal("Good job!", "You clicked the delete button!", "warning");
   }
 
 }
